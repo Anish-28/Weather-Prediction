e7308435d7d47f9517f53db03d141c158ac09757
Anish Mahale <anish.mahale888@gmail.com>
2021-05-07 00:02:44 +0530

    sixth commit

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var unirest = require('unirest');
-var express = require('express');
+const unirest = require('unirest');
+const express = require('express');
 const bodyParser=require("body-parser");
-var app = express();
+const app = express();
 const path = require('path');
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', function(req, res){
-  var query = req.body.cityName;
+app.post("/", function(req, res){
+  const query = req.body.cityName;
   console.log(query);
 unirest.get("https://community-open-weather-map.p.rapidapi.com/forecast")
   .header("X-RapidAPI-Key", "3ad09272cbmsh76449fd82abb8cfp108186jsncd3e9947dafe")
   .header("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
   .query({
-        //'appid': <YOUR_APP_ID>,
         'q': query,
         'units': 'metric'
   })
   .end(function (result) {
-    console.log(result.body.list[0].weather);
+        console.log(result.body.list);
         res.render('index', {data:result.body});
   });
 })
