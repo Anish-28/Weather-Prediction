c6c22e79cf26b1ec7cde2b3d69b35bf60ce6f89a
Anish Mahale <anish.mahale888@gmail.com>
2021-05-07 14:26:26 +0530

    eighth commit

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 const unirest = require('unirest');
 const express = require('express');
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const path = require('path');
 
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'assets')));
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   const query = req.body.cityName;
   console.log(query);
-unirest.get("https://community-open-weather-map.p.rapidapi.com/forecast")
-  .header("X-RapidAPI-Key", "3ad09272cbmsh76449fd82abb8cfp108186jsncd3e9947dafe")
-  .header("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
-  .query({
-        'q': query,
-        'units': 'metric'
-  })
-  .end(function (result) {
-        console.log(result.body.list);
-        res.render('index', {data:result.body});
-  });
+  unirest.get("https://community-open-weather-map.p.rapidapi.com/forecast")
+    .header("X-RapidAPI-Key", "3ad09272cbmsh76449fd82abb8cfp108186jsncd3e9947dafe")
+    .header("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+    .query({
+      'q': query,
+      'units': 'metric'
+    })
+    .end(function(result) {
+      //console.log(result.body.list);
+      res.render('index', {
+        data: result.body
+      });
+    });
 })
-app.listen(process.env.PORT || 8081, function(){
+app.listen(process.env.PORT || 8081, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
